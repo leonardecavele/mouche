9de10bd23e6a5080807a22172440f488cfbc285b
leonardecavele <leonard.decavele@gmail.com>
2025-05-22 18:47:32 +0200

    behind

---
+++ include/project.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define POSX 0
+#define POSY 1
+
+typedef struct
+{
+    char **data;
+    int width;
+    int height;
+	int poschar[2];
+	int posmch[2];
+} BRD;
+
+#include "character.h"
+#include "get_input.h"
+//etc
